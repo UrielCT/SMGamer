bcd2c7c43b5db4be5eb28468fa792d23d3e50dd1
Uriel Thoma <urielct2022@gmail.com>
2025-07-29 13:04:18 -0300

    Create ChatsScreen

--- app/src/main/java/com/smgamer/ui/screens/chats/ChatsScreen.kt
+++ app/src/main/java/com/smgamer/ui/screens/chats/ChatsScreen.kt
@@ -3,21 +3,55 @@ package com.smgamer.ui.screens.chats
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.smgamer.ui.theme.Green100
+import androidx.compose.ui.tooling.preview.Preview
+import com.smgamer.R
+import com.smgamer.ui.components.ChatsCardItem
+import com.smgamer.ui.theme.SMGamerTheme
 import com.smgamer.ui.theme.Yellow100
+
+data class Contact(
+    val name: String,
+    val lastMessage: String,
+    val unreadCount: Int,
+    val profileImageRes: Int
+)
+
 
 @Composable
 fun ChatsScreen(modifier: Modifier){
     Column (modifier = modifier
         .fillMaxSize()
-        .background(Yellow100)
-        , horizontalAlignment = Alignment.CenterHorizontally,
-        //.padding(dimensionResource(R.dimen.common_padding_min))
+        .background(Yellow100),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ){
-        Text("Filters")
+        val contactsList:List<Contact> = listOf(
+            Contact(name = "Juan", lastMessage = "Hola", unreadCount = 3, profileImageRes = R.drawable.ic_person),
+            Contact(name = "Maria", lastMessage = "Hosdfasdg", unreadCount = 300, profileImageRes = R.drawable.ic_person),
+            Contact(name = "Pablo", lastMessage = "Hogala", unreadCount = 0, profileImageRes = R.drawable.ic_person),
+            Contact(name = "Mati", lastMessage = "Hofla", unreadCount = 1000, profileImageRes = R.drawable.ic_person),
+            Contact(name = "Tomi", lastMessage = "Hffffffa", unreadCount = 0, profileImageRes = R.drawable.ic_person)
+        )
+
+        contactsList.forEach { filter ->
+            ChatsCardItem(
+                name = filter.name,
+                lastMessage = filter.lastMessage,
+                unreadCount = filter.unreadCount,
+                profileImageRes = filter.profileImageRes,
+                modifier = Modifier
+            )
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChatsScreenPreview(){
+    SMGamerTheme {
+        ChatsScreen(Modifier)
+    }
+
 }
