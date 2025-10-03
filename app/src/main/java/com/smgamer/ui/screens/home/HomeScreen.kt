c3e0b521180f280efa4dc6baf0085bc6ee546278
Uriel Thoma <urielct2022@gmail.com>
2025-10-03 17:13:00 -0300

    Update UI

--- app/src/main/java/com/smgamer/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/smgamer/ui/screens/home/HomeScreen.kt
@@ -1,16 +1,13 @@
 package com.smgamer.ui.screens.home
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.smgamer.ui.components.PostCard
 import com.smgamer.ui.theme.Background
-import com.smgamer.ui.theme.SMGamerTheme
 import com.smgamer.ui.viewmodels.PostsViewModel
 
 
@@ -22,11 +19,8 @@ fun HomeScreen(
 ){
     LazyColumn(modifier = modifier
         .fillMaxSize()
-        .background(Background)
-        //.padding(dimensionResource(R.dimen.common_padding_min))
-        ,
+        .background(Background),
     horizontalAlignment = Alignment.CenterHorizontally,
-        //verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.common_padding_min))
     ) {
         items(5) {
             PostCard(
@@ -37,13 +31,3 @@ fun HomeScreen(
     }
 
 }
-
-//@SuppressLint("ViewModelConstructorInComposable")
-//@Preview(showBackground = true)
-//@Composable
-//fun HomeScreenPreview(){
-//    SMGamerTheme {
-//        val postsViewModel:PostsViewModel= PostsViewModel()
-//        HomeScreen(Modifier,postsViewModel,{})
-//    }
-//}
